b7d90ca5afd55cf25c8c87b8ed90867248bd234a
Artem Grishin <artem.grishin322@gmail.com>
2021-11-02 12:09:50 +0300

    HW04 Base done

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -49,8 +49,7 @@ public class JdbcMealRepository implements MealRepository {
             Number newId = insertMeal.executeAndReturnKey(map);
             meal.setId(newId.intValue());
         } else {
-            if (namedParameterJdbcTemplate.update("" +
-                    "UPDATE meals " +
+            if (namedParameterJdbcTemplate.update("UPDATE meals " +
                     "   SET description=:description, calories=:calories, date_time=:date_time " +
                     " WHERE id=:id AND user_id=:user_id", map) == 0) {
                 return null;
