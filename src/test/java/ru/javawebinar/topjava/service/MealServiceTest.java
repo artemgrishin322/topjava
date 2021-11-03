9d63c203739633989c21da7acd8f12ae0fe884d3
Artem Grishin <artem.grishin322@gmail.com>
2021-11-03 12:31:03 +0300

    Rollback to base HW04

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,11 +1,16 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.Stopwatch;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,9 +19,11 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
@@ -30,27 +37,6 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
-
-    @Rule
-    public TestRule watcher = new TestWatcher() {
-        private long startTime;
-
-        @Override
-        protected void starting(Description description) {
-            System.out.printf("Test method %s starts%n" +
-                    "==========================================================================================%n",
-                    description.getMethodName());
-            startTime = System.currentTimeMillis();
-        }
-
-        @Override
-        protected void finished(Description description) {
-            long endTime = System.currentTimeMillis() - startTime;
-            System.out.printf("Test method %s completion took %d ms%n" +
-                    "==========================================================================================%n",
-                    description.getMethodName(), endTime);
-        }
-    };
 
     @Autowired
     private MealService service;
